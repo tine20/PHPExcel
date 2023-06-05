319091e92f505ceb26d811354ec6748bab7f904a
Philipp Schüle <p.schuele@metaways.de>
2023-06-05 13:43:50 +0200

    tweak(php 8.1): prevent 'Implicit conversion loses precision' error

--- Classes/PHPExcel/Cell.php
+++ Classes/PHPExcel/Cell.php
@@ -842,10 +842,10 @@ class PHPExcel_Cell
             if ($pColumnIndex < 26) {
                 $_indexCache[$pColumnIndex] = chr(65 + $pColumnIndex);
             } elseif ($pColumnIndex < 702) {
-                $_indexCache[$pColumnIndex] = chr(64 + ($pColumnIndex / 26)) .
+                $_indexCache[$pColumnIndex] = chr(64 + (int) ($pColumnIndex / 26)) .
                                               chr(65 + $pColumnIndex % 26);
             } else {
-                $_indexCache[$pColumnIndex] = chr(64 + (($pColumnIndex - 26) / 676)) .
+                $_indexCache[$pColumnIndex] = chr(64 + (int) (($pColumnIndex - 26) / 676)) .
                                               chr(65 + ((($pColumnIndex - 26) % 676) / 26)) .
                                               chr(65 + $pColumnIndex % 26);
             }
