178503c5c0fbb046bc499df930355ea037620fc9
Paul Mehrer <p.mehrer@metaways.de>
2026-02-26 15:07:26 +0100

    php85 applied rename cast rector

--- .rector.php
+++ .rector.php
@@ -2,9 +2,14 @@
 
 declare(strict_types=1);
 
+use PhpParser\Node\Expr\Cast\Double;
+use PhpParser\Node\Expr\Cast\Int_;
+use PhpParser\Node\Expr\Cast\Bool_;
+use PhpParser\Node\Expr\Cast\String_;
 use Rector\Config\RectorConfig;
 use Rector\CodeQuality\Rector as CodeQuality;
-use Rector\Set\ValueObject\LevelSetList;
+use Rector\Renaming\Rector\Cast\RenameCastRector;
+use Rector\Renaming\ValueObject\RenameCast;
 use Rector\ValueObject\PhpVersion;
 
 return RectorConfig::configure()
@@ -12,6 +17,14 @@ return RectorConfig::configure()
         __DIR__ . '/Classes/',
     ])
     ->withRules([
+        Rector\Php85\Rector\Switch_\ColonAfterSwitchCaseRector::class,
         Rector\Php84\Rector\Param\ExplicitNullableParamTypeRector::class,
+        Rector\Php85\Rector\ShellExec\ShellExecFunctionCallOverBackticksRector::class,
+    ])->withConfiguredRule(RenameCastRector::class, [
+        new RenameCast(Double::class, Double::KIND_REAL, Double::KIND_FLOAT),
+        new RenameCast(Double::class, Double::KIND_DOUBLE, Double::KIND_FLOAT),
+        new RenameCast(Int_::class, Int_::KIND_INTEGER, Int_::KIND_INT),
+        new RenameCast(Bool_::class, Bool_::KIND_BOOLEAN, Bool_::KIND_BOOL),
+        new RenameCast(String_::class, String_::KIND_BINARY, String_::KIND_STRING),
     ])
-    ->withPhpVersion(PhpVersion::PHP_84);
+    ->withPhpVersion(PhpVersion::PHP_85);
